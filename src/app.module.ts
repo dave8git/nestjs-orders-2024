4e1a78ab3abb3d25156ca0500311918b3b5af4e8
dave8git <dave8git@gmail.com>
2024-12-13 15:20:12 +0100

    Add initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+// in this file all modules come together
 @Module({
   imports: [],
   controllers: [AppController],
